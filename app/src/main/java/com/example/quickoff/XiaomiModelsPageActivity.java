6b30d4c61c1bdd3d0aeeb454ef70f87dde97ccfb
Melo <31557544+fatcha14@users.noreply.github.com>
2019-04-08 01:57:21 -0400

    add expandable list view for xiaomi products
    Signed-off-by: Melo <31557544+fatcha14@users.noreply.github.com>

---
+++ app/src/main/java/com/example/quickoff/XiaomiModelsPageActivity.java
@@ -0,0 +1,42 @@
+package com.example.quickoff;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class XiaomiModelsPageActivity extends AppCompatActivity {
+
+    private ExpandableListView listView;
+    private ExpandableListAdapter listAdapter;
+    private List<String> models;
+    private HashMap<String,List<String>> listHash;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apple_models_page);
+
+        listView = (ExpandableListView)findViewById(R.id.models_list);
+        initData();
+        listAdapter = new MyExpandableListAdapter(this, models,listHash);
+        listView.setAdapter(listAdapter);
+    }
+
+    private void initData() {
+        models = new ArrayList<>();
+        listHash = new HashMap<>();
+
+        models.add("Mi9");
+
+        List<String> mi_9 = new ArrayList<>();
+        mi_9.add("128GB");
+        mi_9.add("256GB");
+
+        listHash.put(models.get(0),mi_9);
+
+    }
+}
