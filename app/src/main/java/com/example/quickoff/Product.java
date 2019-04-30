bb60e351ed1d711b13cce904c09ef3ea2db02f95
qxc101 <48734272+qxc101@users.noreply.github.com>
2019-04-30 09:42:35 +0800

    added comments to product.java

--- app/src/main/java/com/example/quickoff/Product.java
+++ app/src/main/java/com/example/quickoff/Product.java
@@ -1,17 +1,20 @@
 package com.example.quickoff;
 
+// a class that represent a product on the market
 public class Product {
     private boolean source; // when this boolean field contains TRUE, then the product is from Amazon.If this field contains FALSE,then the product is from Tmall.
-    private String name;
-    private String price;
-    private String description;
+    private String name;    // name of the product
+    private String price;   // price of the product
+    private String description;     // storage of the product
 
+    // constructor method with no inputs
     public Product(){
         source = false;
         name = null;
 
     }
 
+    // constructor method with all the inputs
     public Product(boolean input_source, String input_name, String input_price){
         source = input_source;
         name = input_name;
@@ -19,34 +22,35 @@ public class Product {
 
     }
 
+    // getter method of source
     public boolean getSource(){
         return this.source;
     }
-
+    // setter method of source
     public void setSource(boolean input){
         this.source = input;
     }
-
+    // getter method of name
     public String getName(){
      return this.name;
     }
-
+    // setter method of name
     public void setName(String input){
         this.name = input;
     }
-
+    // getter method of price
     public String getPrice(){
         return this.price;
     }
-
+    // setter method of price
     public void setPrice(String input){
         this.price = input;
     }
-
+    // getter method of storage
     public String getDescription(){
         return this.description;
     }
-
+    // setter method of storage
     public void setDescription(String input){
         this.description = input;
     }
